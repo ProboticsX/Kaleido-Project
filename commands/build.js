649176aa63f7ee7f633d90e94af16da8d86227f9
ProboticsX <jainshubham0403@gmail.com>
2022-10-01 16:13:18 -0400

    Add files

--- commands/build.js
+++ commands/build.js
@@ -7,10 +7,6 @@ const os = require('os');
 const { options } = require("yargs");
 
 const configFile = require("../lib/config");
-const mutationBuild = require("../lib/mutationBuild");
-const petBuild = require("../lib/petBuild");
-const angularBuild = require("../lib/angularBuild");
-
 
 require('dotenv').config();
 
@@ -42,36 +38,6 @@ exports.handler = async argv => {
             return;
         }
 
-        if(job_name=="itrust-build") {
-
-            let cloneCommands = buildYamlFile.jobs[index].clone;  
-            await configFile.runCloneSteps(cloneCommands);
-
-            let sqlSetupSteps = buildYamlFile.jobs[index].sql_setup;   
-            await configFile.runSQLSetupSteps(sqlSetupSteps);
-
-            let runSteps = buildYamlFile.jobs[index].steps;   
-            await configFile.runSteps(runSteps);
-        
-        }
-
-        if(job_name=="mutation-coverage") {
-
-            await mutationBuild.cloneMutationRepositories(buildYamlFile,index);
-            await mutationBuild.createFinalJSON(buildYamlFile,index);
-            await mutationBuild.sendFilesToVM();
-            await mutationBuild.runMutation(buildYamlFile, index);
-        
-        }
-
-        
-        if(job_name == "pet-build"){
-            await petBuild.runPetSteps(buildYamlFile, index);
-        }
-
-        if(job_name == "angular-build"){
-            await angularBuild.runAngularSteps(buildYamlFile, index);
-        }
     } 
     catch (e) 
     {
